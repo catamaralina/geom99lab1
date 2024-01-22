7a52c5cbb9be1926d692dd9e545abb9e39e28463
catamaralina <tali.jonker@gmail.com>
2024-01-22 16:08:55 -0500

    idk

--- story/story.js
+++ story/story.js
@@ -1,110 +1,36 @@
-/*-----
-FROM: https://developers.google.com/maps/documentation/javascript/examples/map-simple#maps_map_simple-javascript
------*/
 function initMap() {
-  const origin = { lat: 34.9875139, lng: 135.7575207 };
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
-    center: origin,
+    center: { lat: 41.85, lng: -87.65 },
   });
 
-  new ClickEventHandler(map, origin);
+  directionsRenderer.setMap(map);
+
+  const onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  };
+
+  document.getElementById("start").addEventListener("change", onChangeHandler);
+  document.getElementById("end").addEventListener("change", onChangeHandler);
 }
 
-function isIconMouseEvent(e) {
-  return "placeId" in e;
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  directionsService
+    .route({
+      origin: {
+        query: document.getElementById("start").value,
+      },
+      destination: {
+        query: document.getElementById("end").value,
+      },
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + status));
 }
 
-class ClickEventHandler {
-  origin;
-  map;
-  directionsService;
-  directionsRenderer;
-  placesService;
-  infowindow;
-  infowindowContent;
-  constructor(map, origin) {
-    this.origin = origin;
-    this.map = map;
-    this.directionsService = new google.maps.DirectionsService();
-    this.directionsRenderer = new google.maps.DirectionsRenderer();
-    this.directionsRenderer.setMap(map);
-    this.placesService = new google.maps.places.PlacesService(map);
-    this.infowindow = new google.maps.InfoWindow();
-    this.infowindowContent = document.getElementById("infowindow-content");
-    this.infowindow.setContent(this.infowindowContent);
-    // Listen for clicks on the map.
-    this.map.addListener("click", this.handleClick.bind(this));
-  }
-  handleClick(event) {
-    console.log("You clicked on: " + event.latLng);
-    // If the event has a placeId, use it.
-    if (isIconMouseEvent(event)) {
-      console.log("You clicked on place:" + event.placeId);
-      // Calling e.stop() on the event prevents the default info window from
-      // showing.
-      // If you call stop here when there is no placeId you will prevent some
-      // other map click event handlers from receiving the event.
-      event.stop();
-      if (event.placeId) {
-        this.calculateAndDisplayRoute(event.placeId);
-        this.getPlaceInformation(event.placeId);
-      }
-    }
-  }
-  calculateAndDisplayRoute(placeId) {
-    const me = this;
-
-    this.directionsService
-      .route({
-        origin: this.origin,
-        destination: { placeId: placeId },
-        travelMode: google.maps.TravelMode.WALKING,
-      })
-      .then((response) => {
-        me.directionsRenderer.setDirections(response);
-      })
-      .catch((e) => window.alert("Directions request failed due to " + status));
-  }
-  getPlaceInformation(placeId) {
-    const me = this;
-
-    this.placesService.getDetails({ placeId: placeId }, (place, status) => {
-      if (
-        status === "OK" &&
-        place &&
-        place.geometry &&
-        place.geometry.location
-      ) {
-        me.infowindow.close();
-        me.infowindow.setPosition(place.geometry.location);
-        me.infowindowContent.children["place-icon"].src = place.icon;
-        me.infowindowContent.children["place-name"].textContent = place.name;
-        me.infowindowContent.children["place-id"].textContent = place.place_id;
-        me.infowindowContent.children["place-address"].textContent =
-          place.formatted_address;
-        me.infowindow.open(me.map);
-      }
-    });
-  }
-}
-
-window.initMap = initMap;
-
-
-async function initMap() {
-  // Request needed libraries.
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const map = new Map(document.getElementById("map"), {
-    center: { lat: 34.9660541, lng: 135.6445377 },
-    zoom: 14,
-    mapId: "Fushimi+Inari+Taisha",
-  });
-  const marker = new AdvancedMarkerElement({
-    map,
-    position: { lat: 34.9660541, lng: 135.6445377 },
-  });
-}
-
-initMap();
+window.initMap = initMap; 
