cefdc4998b0d1d4fa70c08dcdc843299a137343c
catamaralina <tali.jonker@gmail.com>
2024-01-23 15:21:02 -0500

    chaos and  uselessness

--- story/story.js
+++ story/story.js
@@ -1,36 +1,71 @@
-function initMap() {
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
-    center: { lat: 34.9875139, lng: 135.7575207 },
+async function initMap() {
+  // Request needed libraries.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker",
+  );
+  const map = new Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 34.9862398, lng: 135.7569057 },
+    mapId: "4504f8b37365c3d0",
   });
+  // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+  // receives the initial focus when tab is pressed. Use arrow keys to
+  // move between markers; press tab again to cycle through the map controls.
+  const tourStops = [
+    {
+      position: { lat: 34.9893453, lng: 135.7566664 },
+      title: "Kyoto Tower",
+    },
+    {
+      position: { lat: 34.991349, lng: 135.758469 },
+      title: "Tokiwacho",
+    },
+    {
+      position: { lat: 34.9873555, lng: 135.7461619},
+      title: "Kyoto Aquarium",
+    },
+    {
+      position: { lat: 34.9983422, lng: 135.771512 },
+      title: "Maruyama Park",
+    },
+    {
+      position: { lat: 35.0228405, lng: 135.8049198 },
+      title: "Gozan no Okuribi",
+    },
+    {
+      position: { lat: 35.0376503, lng: 135.7604489 },
+      title: "Kamogawa Park",
+    },
+    {
+      position: { lat: 35.021703, lng: 135.757493 },
+      title: "Kyoto National Garden",
+    },
+  ];
+  // Create an info window to share between markers.
+  const infoWindow = new InfoWindow();
 
-  directionsRenderer.setMap(map);
+  // Create the markers.
+  tourStops.forEach(({ position, title }, i) => {
+    const pin = new PinElement({
+      glyph: `${i + 1}`,
+    });
+    const marker = new AdvancedMarkerElement({
+      position,
+      map,
+      title: `${i + 1}. ${title}`,
+      content: pin.element,
+    });
 
-  const onChangeHandler = function () {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-  };
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", ({ domEvent, latLng }) => {
+      const { target } = domEvent;
 
-  document.getElementById("start").addEventListener("change", onChangeHandler);
-  document.getElementById("end").addEventListener("change", onChangeHandler);
+      infoWindow.close();
+      infoWindow.setContent(marker.title);
+      infoWindow.open(marker.map, marker);
+    });
+  });
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  directionsService
-    .route({
-      origin: {
-        query: document.getElementById("start").value,
-      },
-      destination: {
-        query: document.getElementById("end").value,
-      },
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    .then((response) => {
-      directionsRenderer.setDirections(response);
-    })
-    .catch((e) => window.alert("Directions request failed due to " + status));
-}
-
-window.initMap = initMap;
+initMap();
